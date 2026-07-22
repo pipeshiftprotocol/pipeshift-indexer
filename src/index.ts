4b77d613afc1ed179d2b8da865ffd9d8a7ef79c2
luka <luka@pipeshift.trade>
2026-07-22 10:42:00 +0000

    feat: public entry point

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Pipeshift settlement indexer.
+ *
+ * Decodes settlement and netting events, then aggregates them into positions,
+ * per security totals and compression reports. Read only by construction: there
+ * is no signer here and nothing in this package can send a transaction.
+ */
+
+export * from "./types.js";
+export * from "./decode.js";
+export * from "./aggregate.js";
+export * from "./report.js";
